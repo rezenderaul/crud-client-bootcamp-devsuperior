a7341e85836bb012b2a766057437f53e6dcdeb0f
Raul Cristiano Rezende <raulcristianorezende@outlook.com.br>
2021-05-18 09:05:39 -0300

    Find by id, exception handling

--- src/main/java/com/raulrezende/crudclient/services/ClientService.java
+++ src/main/java/com/raulrezende/crudclient/services/ClientService.java
@@ -1,6 +1,7 @@
 package com.raulrezende.crudclient.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.raulrezende.crudclient.dto.ClientDTO;
 import com.raulrezende.crudclient.entities.Client;
 import com.raulrezende.crudclient.repositories.ClientRepository;
+import com.raulrezende.crudclient.services.exception.EntityNotFoundException;
 
 @Service
 public class ClientService {
@@ -21,5 +23,11 @@ public class ClientService {
 	public List<ClientDTO> findAll() {
 		List<Client> list = repository.findAll();
 		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+	}
+
+	public ClientDTO findById(Long id) {
+		Optional<Client> obj = repository.findById(id);
+		Client entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		return new ClientDTO(entity);
 	}
 }
---
+++ src/main/java/com/raulrezende/crudclient/services/exception/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.raulrezende.crudclient.services.exception;
+
+public class EntityNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public EntityNotFoundException(String msg) {
+		super(msg);
+	}
+}
